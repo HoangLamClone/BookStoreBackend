b5b1b5f7450c35dd1ef32d4cf502926e431bc040
HoangLamClone <maituanan7061@gmail.com>
2024-05-24 00:15:20 +0700

    Fix Register

--- BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Requests/CustomerInformationRequest.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Dtos/Requests/CustomerInformationRequest.java
@@ -15,11 +15,9 @@ public class CustomerInformationRequest {
     @NotNull
     @Size(max = 100)
     String fullname;
-    @Email
     String email;
     Boolean gender;
     Date birthday;
-    @Digits(integer = 10,fraction = 0)
     String phonenumber;
     String address;
 }
