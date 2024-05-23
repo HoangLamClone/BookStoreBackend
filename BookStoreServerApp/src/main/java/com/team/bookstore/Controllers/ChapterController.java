cc85f6b03069fca31db6810a1b6432d46a396971
HoangLamClone <maituanan7061@gmail.com>
2024-05-23 21:52:51 +0700

    FixUpdateReadingProcess

--- BookStoreServerApp/src/main/java/com/team/bookstore/Controllers/ChapterController.java
+++ BookStoreServerApp/src/main/java/com/team/bookstore/Controllers/ChapterController.java
@@ -6,6 +6,7 @@ import com.team.bookstore.Dtos.Responses.ChapterResponse;
 import com.team.bookstore.Mappers.ChapterMapper;
 import com.team.bookstore.Services.ChapterService;
 import com.team.bookstore.Services.Customer_BookService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
@@ -21,11 +22,13 @@ public class ChapterController {
     ChapterMapper chapterMapper;
     @Autowired
     Customer_BookService customerBookService;
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/all")
     public ResponseEntity<APIResponse<?>> getAllChapters(){
         return ResponseEntity.ok(APIResponse.builder().code(200).message(
                 "OK").result(chapterService.getAllChapters()).build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/add")
     public ResponseEntity<APIResponse<?>> addChapter(@RequestPart MultipartFile file, @RequestPart ChapterRequest chapterRequest){
         ChapterResponse ressult = chapterService.addChapter(file,
@@ -33,6 +36,7 @@ public class ChapterController {
         return ResponseEntity.ok(APIResponse.builder().code(200).message(
                 "OK").result(ressult).build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @PatchMapping("/update")
     public ResponseEntity<APIResponse<?>> updateChapter(@RequestParam int book_id,
             @RequestPart MultipartFile file, @RequestPart ChapterRequest chapterRequest){
@@ -41,17 +45,20 @@ public class ChapterController {
         return ResponseEntity.ok(APIResponse.builder().code(200).message(
                 "OK").result(ressult).build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @DeleteMapping("/delete")
     public ResponseEntity<APIResponse<?>> deleteChapter(@RequestParam int book_id){
         return ResponseEntity.ok(APIResponse.builder().code(200).message(
                 "OK").result(chapterService.deleteChapter(book_id)).build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @PatchMapping("/update-reading-chaper")
     public ResponseEntity<APIResponse<?>> updateReadingChaper(@RequestParam int book_id,@RequestParam int chapter_index){
         return ResponseEntity.ok(APIResponse.builder().code(200).message(
                 "OK").result(customerBookService.updateReadingProcess(book_id
                 ,chapter_index)).build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/get-reading-chaper")
     public ResponseEntity<APIResponse<?>> getReadingChapter(@RequestParam int book_id){
         return ResponseEntity.ok(APIResponse.builder().code(200).message("OK").result(customerBookService.getReadingProcess(book_id)).build());
